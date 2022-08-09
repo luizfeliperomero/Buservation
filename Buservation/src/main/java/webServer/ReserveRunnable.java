40fff3e9d6e7affdd9ee4dfefeb9285bcdf73069
luiz felipe <luizfeliperomero10@gmail.com>
2022-08-09 19:07:55 -0300

    Fix path

--- Buservation/src/main/java/webServer/ReserveRunnable.java
+++ Buservation/src/main/java/webServer/ReserveRunnable.java
@@ -89,6 +89,8 @@ public class ReserveRunnable implements Runnable {
 
         File file = new File("Buservation" + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + recurso);
 
+		System.Out.Println(file.getAbsolutePath());
+
         if (file.exists()) {
             Path path = file.toPath();
             String mimeType = null;
